cb0dbfe4d714fac125b6ec747eccdf3ee3a0c031
ESCORNEBOUEU <titouanescorneboueu@gmail.com>
2021-02-18 16:17:49 +0100

    Debut des commentaire et installation des router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
+// ! Importation de useState pour pouvoir modifier le contenu que je souhaite
 import React, { useState } from "react";
 import "./App.css";
+// ! Importation de mes données pour ma boucle
 import data from "./data";
+// ! Importation de ma Nav Bar
 import NavBar from "./components/NavBar";
+// ! Importation de mon component Person
 import Person from "./components/Person";
+// ! Importation de mon component Body
 import Body from "./components/Body";
+// ! Importation pour le Router
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// ! Importation de mes pages en js
+import Home from "./pages/Home";
+import Projets from "./pages/Projets";
+import Contact from "./pages/Contact";
 
 function App() {
+  // ? Déclaration d'une constante pour le useState qui modiferie la data
   const [anonyme, setAnonyme] = useState(data);
   return (
-    <main>
-      <NavBar />
-      <div className="bas">
-        <Person anonyme={anonyme} />
-      </div>
-      <Body />
-    </main>
+    // !Appel de la balise Route
+    <Router>
+      <main>
+        <NavBar />
+        {/* Appel de la balise Switch donc ce qui va changer */}
+        <Switch>
+          {/* Appel de la balise Route ou je vais faire le lien avec ma page js */}
+          <Route path="/" component={Home}>
+            <div className="bas">
+              {/* Appel de ma constante */}
+              <Person anonyme={anonyme} />
+            </div>
+          </Route>
+          <Route path="/projets" component={Projets} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+        <Body />
+      </main>
+    </Router>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,18 @@
+// ! imporation de Link pour le lien
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
   return (
     <nav>
-      <a href="">
-        <img
-          src="https://images.pexels.com/photos/4587971/pexels-photo-4587971.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-          alt="Un putain de chien trop mignon"
-        />
-      </a>
+      {/* Appel de la balise link pour marquer l'endroit ou quand je vais appuyer dessus le lien va se faire */}
+      <Link to="#">
+        <a href="">
+          <img
+            src="https://images.pexels.com/photos/4587971/pexels-photo-4587971.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            alt="Un putain de chien trop mignon"
+          />
+        </a>
+      </Link>
       <div>
         <a href="Person.jsx">Projets</a>
         <a href="">Contact</a>
