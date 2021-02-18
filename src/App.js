81178d420aeba6d181bc76b8f560a46f81011479
ESCORNEBOUEU <titouanescorneboueu@gmail.com>
2021-02-18 13:25:36 +0100

    Creation de la navBar

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import React, { useState } from "react";
 import "./App.css";
 import data from "./data";
 import List from "./components/List";
+import NavBar from "./components/NavBar";
 
 function App() {
   const [student, setStudent] = useState(data);
 
   return (
     <main>
-      <div className="button">
-        <button onClick={() => setStudent([])}>Clique</button>
-      </div>
+      <NavBar />
       <section className="container">
-        <h2>Attendance list for {student.length} students</h2>
-        <List student={student} />
+        <div className="button">
+          <button onClick={() => setStudent([])}>Clique</button>
+        </div>
+        <div className="nom_des_eleves">
+          <h2>Attendance list for {student.length} students</h2>
+          <List student={student} />
+        </div>
+        <div className="button">
+          <button onClick={() => setStudent([])}>Clear list</button>
+        </div>
       </section>
-      <div className="button">
-        <button onClick={() => setStudent([])}>Clear list</button>
-      </div>
     </main>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <a href="">
+        <img
+          src="https://images.pexels.com/photos/4587971/pexels-photo-4587971.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          alt="Un putain de chien trop mignon"
+        />
+      </a>
+      <div>
+        <a href="">Projets</a>
+        <a href="">Contact</a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
